37c0df2e36dc970346bb7ca40deec43a001654b7
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-08 23:20:50 +0700

    fix err config

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
+
   validates :name, presence: true
   has_many :categories
   has_many :trades, foreign_key: 'author_id', class_name: 'Trade', dependent: :destroy
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,19 +91,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://mail.google.com' }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host: 'https://mail.google.com' }
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    domain: 'mail.google.com',
-    user_name: 'kevinphandeveloper@gmail.com',
-    password: ENV['EMAIL_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: '587',
+  #   domain: 'mail.google.com',
+  #   user_name: 'kevinphandeveloper@gmail.com',
+  #   password: ENV['EMAIL_PASSWORD'],
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
 end
