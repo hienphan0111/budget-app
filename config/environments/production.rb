4d1fed3cab5b53a542291684e5d28d0e99bc1084
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-08 21:59:47 +0700

    Add mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'info.camerahd',
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'gmail.com' }
+
 end
