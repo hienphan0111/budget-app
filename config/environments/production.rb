7f0af76d67924350563482b2173c5edabec386bc
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-08 22:45:27 +0700

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,16 +91,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://mail.google.com' }
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'info.camerahd@gmail.com',
+    port: '587',
+    domain: 'mail.google.com',
+    user_name: 'kevinphandeveloper@gmail.com',
     password: ENV['EMAIL_PASSWORD'],
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true
   }
-
-  config.action_mailer.default_url_options = { host: 'gmail.com' }
 
 end
